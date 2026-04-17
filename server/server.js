345f66f0f747ea95cfdb9b33edbbd1186192b77d
thienmta <thienmta.py@gmail.com>
2026-04-17 10:23:45 +0700

    fix: server with CORS

--- server/server.js
+++ server/server.js
@@ -11,9 +11,17 @@ const booksRoutes = require("./routes/book");
 const PORT = process.env.PORT || 5000;
 
 // Middleware chung
+const allowedOrigins = ['localhost', 'vercel.app', 'netlify.app', 'github.io'];
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    // allow localhost with any port, vercel, netlify, and github.io
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.some(allowedOrigin => origin.includes(allowedOrigin))) {
+	callback(null, true);
+      } else {
+	callback(new Error('Not allowed by CORS'));
+      }
+    },
   })
 );
 
